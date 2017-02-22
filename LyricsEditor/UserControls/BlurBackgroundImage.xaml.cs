1cb9119d82301a2c9350de68e5f7defb0b7ccf01
Mengxu <taomengxu@126.com>
2017-02-22 10:56:40 +0800

    动态加载程序集，用以支持高版本设备的背景模糊效果

--- LyricsEditor/UserControls/BlurBackgroundImage.xaml.cs
+++ LyricsEditor/UserControls/BlurBackgroundImage.xaml.cs
@@ -1,19 +1,7 @@
-﻿using LyricsEditor.Model;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
+﻿using System;
+using System.Reflection;
+using LyricsEditor.Model;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Media.Imaging;
-using Windows.UI.Xaml.Navigation;
 
 // The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
 
@@ -21,11 +9,51 @@ namespace LyricsEditor.UserControls
 {
     public sealed partial class BlurBackgroundImage : UserControl
     {
+        private static Type typeBlur = GetBlurType();
+
+        private static Type GetBlurType()
+        {
+            Type typeBlur = null;
+            try
+            {
+                typeBlur = Assembly.Load(new AssemblyName("Microsoft.Toolkit.Uwp.UI.Animations"))
+                    .GetType("Microsoft.Toolkit.Uwp.UI.Animations.Behaviors.Blur");
+            }
+            catch (Exception)
+            {
+                // Cannot load the assembly or get the type
+                // we rethrow the exception for debugging
+                throw;
+            }
+            return typeBlur;
+        }
+
+        private void SetBlurToSelf()
+        {
+            if (typeBlur != null)
+            {
+                var blurObj = typeBlur.GetConstructor(new Type[] { }).Invoke(new object[] { });
+                typeBlur.GetProperty("Value").SetValue(blurObj, settings.BackgroundBlurDegree);
+                typeBlur.GetProperty("Duration").SetValue(blurObj, 0);
+                typeBlur.GetProperty("AutomaticallyStart").SetValue(blurObj, true);
+                typeBlur.GetMethod("Attach").Invoke(blurObj, new object[] { image });
+            }
+            //var blur = new Blur()
+            //{
+            //    Value = settings.BackgroundBlurDegree,
+            //    Duration = 0,
+            //    AutomaticallyStart = true
+            //};
+            //blur.Attach(image);
+        }
+
+
         private Setting settings;
         public BlurBackgroundImage()
         {
             this.InitializeComponent();
             settings = Setting.GetSettingObject();
+            this.SetBlurToSelf();
         }
     }
 }
