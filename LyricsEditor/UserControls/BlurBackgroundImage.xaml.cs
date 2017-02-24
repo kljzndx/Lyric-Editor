c610007d67bffff265c42a3f2db8ac17ef94d3b1
李继龙 <kljzndx@outlook.com>
2017-02-24 15:50:49 +0800

    去除动态加载程序集

--- LyricsEditor/UserControls/BlurBackgroundImage.xaml.cs
+++ LyricsEditor/UserControls/BlurBackgroundImage.xaml.cs
@@ -11,49 +11,49 @@ namespace LyricsEditor.UserControls
 {
     public sealed partial class BlurBackgroundImage : UserControl
     {
-        private static readonly Type typeBlur = GetBlurType();
-        private static Type GetBlurType()
-        {
-            Type _typeBlur = null;
-            try
-            {
-                _typeBlur = Assembly.Load(new AssemblyName("Microsoft.Toolkit.Uwp.UI.Animations"))
-                    .GetType("Microsoft.Toolkit.Uwp.UI.Animations.Behaviors.Blur");
-            }
-            catch (Exception)
-            {
-                // Cannot load the assembly or get the type
-                // we rethrow the exception for debugging
-                //throw;
-            }
-            return _typeBlur;
-        }
+        //private static readonly Type typeBlur = GetBlurType();
+        //private static Type GetBlurType()
+        //{
+        //    Type _typeBlur = null;
+        //    try
+        //    {
+        //        _typeBlur = Assembly.Load(new AssemblyName("Microsoft.Toolkit.Uwp.UI.Animations"))
+        //            .GetType("Microsoft.Toolkit.Uwp.UI.Animations.Behaviors.Blur");
+        //    }
+        //    catch (Exception)
+        //    {
+        //        // Cannot load the assembly or get the type
+        //        // we rethrow the exception for debugging
+        //        // throw;
+        //    }
+        //    return _typeBlur;
+        //}
 
-        private void SetBlurToSelf()
-        {
-            if (typeBlur != null)
-            {
-                var blurObj = typeBlur.GetConstructor(Type.EmptyTypes).Invoke(null);
-                var valueprop = typeBlur.GetProperty("Value");
-                valueprop.SetValue(blurObj, settings.BackgroundBlurDegree);
-                typeBlur.GetProperty("Duration").SetValue(blurObj, 0);
-                typeBlur.GetProperty("AutomaticallyStart").SetValue(blurObj, true);
-                typeBlur.GetMethod("Attach").Invoke(blurObj, new object[] { image });
+        //private void SetBlurToSelf()
+        //{
+        //    if (typeBlur != null)
+        //    {
+        //        var blurObj = typeBlur.GetConstructor(Type.EmptyTypes).Invoke(null);
+        //        var valueprop = typeBlur.GetProperty("Value");
+        //        valueprop.SetValue(blurObj, settings.BackgroundBlurDegree);
+        //        typeBlur.GetProperty("Duration").SetValue(blurObj, 0);
+        //        typeBlur.GetProperty("AutomaticallyStart").SetValue(blurObj, true);
+        //        typeBlur.GetMethod("Attach").Invoke(blurObj, new object[] { image });
 
-                settings.PropertyChanged += (s, e) => 
-                {
-                    if (e.PropertyName == "BackgroundBlurDegree")
-                        valueprop.SetValue(blurObj, settings.BackgroundBlurDegree);
-                };
-            }
-            //var blur = new Blur()
-            //{
-            //    Value = settings.BackgroundBlurDegree,
-            //    Duration = 0,
-            //    AutomaticallyStart = true
-            //};
-            //blur.Attach(image);
-        }
+        //        settings.PropertyChanged += (s, e) => 
+        //        {
+        //            if (e.PropertyName == "BackgroundBlurDegree")
+        //                valueprop.SetValue(blurObj, settings.BackgroundBlurDegree);
+        //        };
+        //    }
+        //    //var blur = new Blur()
+        //    //{
+        //    //    Value = settings.BackgroundBlurDegree,
+        //    //    Duration = 0,
+        //    //    AutomaticallyStart = true
+        //    //};
+        //    //blur.Attach(image);
+        //}
 
 
         private Setting settings;
@@ -61,7 +61,7 @@ namespace LyricsEditor.UserControls
         {
             this.InitializeComponent();
             this.settings = Setting.GetSettingObject();
-            this.SetBlurToSelf();
+            //this.SetBlurToSelf();
         }
     }
 }
