701659a4d44d3d96d01653590ae2ec0a6878e577
李继龙 <kljzndx@outlook.com>
2017-03-05 14:05:30 +0800

    更改版本号

--- LyricsEditor/Auxiliary/CharacterLibrary.cs
+++ LyricsEditor/Auxiliary/CharacterLibrary.cs
@@ -9,6 +9,9 @@ namespace LyricsEditor.Auxiliary
 {
     public static class CharacterLibrary
     {
+        public static ResourceLoader Main { get; } = ResourceLoader.GetForCurrentView("Main");
+        public static ResourceLoader Setting { get; } = ResourceLoader.GetForCurrentView("Setting");
         public static ResourceLoader MessageBox { get; } = ResourceLoader.GetForCurrentView("MessageBox");
+        public static ResourceLoader ShortcutKeys { get; } = ResourceLoader.GetForCurrentView("ShortcutKeys");
     }
 }
