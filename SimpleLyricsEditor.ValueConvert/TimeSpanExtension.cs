c975fd2f77b11e4f2e309a796097b4bd5993669b
李继龙 <kljzndx@outlook.com>
2017-08-11 07:53:52 +0800

    Add TimeSpanExtension

---
+++ SimpleLyricsEditor.ValueConvert/TimeSpanExtension.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SimpleLyricsEditor.ValueConvert
+{
+    public static class TimeSpanExtension
+    {
+        public static string ToLyricTimeString(this TimeSpan time)
+        {
+            return $"{time.Minutes:D2}:{time.Seconds:D2}.{time.Milliseconds.ToString("D3").Remove(2)}";
+        }
+    }
+}
