f1dd82c2c51e59237ec54efc4627eeab325edfe3
李继龙 <kljzndx@outlook.com>
2017-08-11 08:10:44 +0800

    Add Converts

---
+++ SimpleLyricsEditor.ValueConvert/TimeSpanToDouble.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Windows.UI.Xaml.Data;
+
+namespace SimpleLyricsEditor.ValueConvert
+{
+    public class TimeSpanToDouble : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            return ((TimeSpan) value).TotalMinutes;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            return TimeSpan.FromMinutes((double) value);
+        }
+    }
+}
---
+++ SimpleLyricsEditor.ValueConvert/TimeSpanToLyricTimeString.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Windows.UI.Xaml.Data;
+
+namespace SimpleLyricsEditor.ValueConvert
+{
+    public class TimeSpanToLyricTimeString : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            return ((TimeSpan) value).ToLyricTimeString();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
