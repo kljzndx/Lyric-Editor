084751dcfc05c2c1992f90d6b60df223f38be664
李继龙 <kljzndx@outlook.com>
2017-08-11 16:51:33 +0800

    Add Music Model

---
+++ SimpleLyricsEditor.DAL/Music.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading.Tasks;
+using Windows.Storage;
+using Windows.Storage.FileProperties;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace SimpleLyricsEditor.DAL
+{
+    public class Music
+    {
+        private Music()
+        {
+            Name = string.Empty;
+            Artist = string.Empty;
+            Album = string.Empty;
+            AlbumImage = new BitmapImage();
+            AllTime = TimeSpan.Zero;
+        }
+
+        private Music(string name, string artist, string album, BitmapSource albumImage, TimeSpan allTime, StorageFile file)
+        {
+            Name = name;
+            Artist = artist;
+            Album = album;
+            AlbumImage = albumImage;
+            AllTime = allTime;
+            File = file;
+        }
+
+        public static Music Empty { get; } = new Music();
+
+        public string Name { get; set; }
+        public string Artist { get; set; }
+        public string Album { get; set; }
+        public BitmapSource AlbumImage { get; set; }
+        public TimeSpan AllTime { get; set; }
+        public StorageFile File { get; set; }
+
+        public static async Task<Music> Parse(StorageFile file)
+        {
+            var musicProperties = await file.Properties.GetMusicPropertiesAsync();
+            var image = new BitmapImage();
+            image.SetSource(await file.GetThumbnailAsync(ThumbnailMode.MusicView));
+            return new Music(musicProperties.Title, musicProperties.Artist, musicProperties.Album, image, musicProperties.Duration, file);
+        }
+    }
+}
