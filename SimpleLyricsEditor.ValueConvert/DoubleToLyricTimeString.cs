278bafb3e60b856fa472821d8ab75fcbd28f183f
李继龙 <kljzndx@outlook.com>
2017-08-17 17:47:43 +0800

    Add "DoubleToLyricTimeString" Converter

---
+++ SimpleLyricsEditor.ValueConvert/DoubleToLyricTimeString.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Windows.UI.Xaml.Data;
+
+namespace SimpleLyricsEditor.ValueConvert
+{
+    public class DoubleToLyricTimeString : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            return TimeSpan.FromMinutes((double) value).ToLyricTimeString();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
