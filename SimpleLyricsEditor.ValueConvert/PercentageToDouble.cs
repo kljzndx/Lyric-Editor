327ef546f0d252d91e913510771c1a29ff145fc5
李继龙 <kljzndx@outlook.com>
2017-08-20 13:26:42 +0800

    Fix bugs

--- SimpleLyricsEditor.ValueConvert/PercentageToDouble.cs
+++ SimpleLyricsEditor.ValueConvert/PercentageToDouble.cs
@@ -7,12 +7,12 @@ namespace SimpleLyricsEditor.ValueConvert
     {
         public object Convert(object value, Type targetType, object parameter, string language)
         {
-            return (double) value / 100;
+            return (double) value * 100;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, string language)
         {
-            return (double) value * 100;
+            return (double) value / 100;
         }
     }
 }
