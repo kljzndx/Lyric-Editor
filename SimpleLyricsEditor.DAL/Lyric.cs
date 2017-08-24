b04d7b70efd306c7e58e5e2ec19500bf863ebdc6
李继龙 <kljzndx@outlook.com>
2017-08-24 19:57:02 +0800

    Add class "Lyric"

---
+++ SimpleLyricsEditor.DAL/Lyric.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using SimpleLyricsEditor.IDAL;
+
+namespace SimpleLyricsEditor.DAL
+{
+    public class Lyric : ObservableObject
+    {
+        private string _content;
+        private TimeSpan _time;
+
+        public TimeSpan Time
+        {
+            get => _time;
+            set => Set(ref _time, value);
+        }
+
+        public string Content
+        {
+            get => _content;
+            set => Set(ref _content, value);
+        }
+    }
+}
