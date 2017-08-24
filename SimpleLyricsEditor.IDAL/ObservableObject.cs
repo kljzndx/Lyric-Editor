53cf60bad9d704b972118d97364cee108a6c0dc8
李继龙 <kljzndx@outlook.com>
2017-08-24 19:49:38 +0800

    Add files

---
+++ SimpleLyricsEditor.IDAL/ObservableObject.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace SimpleLyricsEditor.IDAL
+{
+    public abstract class ObservableObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        
+        protected void OnPropertyChanged(string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected void Set<T>(ref T field, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (field.Equals(value))
+                return;
+
+            OnPropertyChanged(propertyName);
+        }
+    }
+}
