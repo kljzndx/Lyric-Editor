93d05fc397e6874aff0f75635df92e2301aea886
李继龙 <kljzndx@outlook.com>
2017-08-27 17:17:12 +0800

    Modify project name

---
+++ SimpleLyricsEditor.EventArguments/LyricsFileChangeEventArgs.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Windows.Storage;
+
+namespace SimpleLyricsEditor.EventArguments
+{
+    public class LyricsFileChangeEventArgs : EventArgs
+    {
+        public LyricsFileChangeEventArgs(StorageFile file)
+        {
+            File = file;
+        }
+
+        public StorageFile File { get; set; }
+    }
+}
--- SimpleLyricsEditor.EventArguments/Properties/AssemblyInfo.cs
+++ SimpleLyricsEditor.EventArguments/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SimpleLyricsEditor.EventArgses")]
+[assembly: AssemblyTitle("SimpleLyricsEditor.EventArguments")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SimpleLyricsEditor.EventArgses")]
+[assembly: AssemblyProduct("SimpleLyricsEditor.EventArguments")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
