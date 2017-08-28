e2fdae18c4be58e1baf75fda58769d68af73cb95
李继龙 <kljzndx@outlook.com>
2017-08-28 09:34:10 +0800

    Remove Unnecessary Usings

--- SimpleLyricsEditor.Events/LyricsFileNotification.cs
+++ SimpleLyricsEditor.Events/LyricsFileNotification.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Runtime.InteropServices.WindowsRuntime;
-using System.Text;
-using System.Threading.Tasks;
 using Windows.Storage;
 
 namespace SimpleLyricsEditor.Events
