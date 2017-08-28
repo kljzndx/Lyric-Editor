a87a005b1e523f6021d9374322c230a5e14304a3
李继龙 <kljzndx@outlook.com>
2017-08-28 08:27:29 +0800

    Added two class "LyricsFileIO" and "LyricsFilePicker"

---
+++ SimpleLyricsEditor.BLL/LyricsFileIO.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+
+namespace SimpleLyricsEditor.BLL
+{
+    public static class LyricsFileIO
+    {
+        private static readonly Encoding GbkEncoding = GetGbkEncoding();
+
+        public static async Task<string> ReadText(IStorageFile file)
+        {
+            string result;
+
+            try
+            {
+                result = await FileIO.ReadTextAsync(file);
+            }
+            catch (Exception)
+            {
+                var buffer = await FileIO.ReadBufferAsync(file);
+                var bytes = buffer.ToArray();
+
+                result = GbkEncoding.GetString(bytes);
+            }
+
+            return result;
+        }
+
+        public static async Task WriteText(StorageFile file, string contents)
+        {
+            await FileIO.WriteTextAsync(file, contents);
+        }
+
+        private static Encoding GetGbkEncoding()
+        {
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            return Encoding.GetEncoding("GBK");
+        }
+    }
+}
---
+++ SimpleLyricsEditor.BLL/LyricsFilePicker.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+
+namespace SimpleLyricsEditor.BLL
+{
+    public static class LyricsFilePicker
+    {
+        private static readonly FileOpenPicker OpenPicker = GetOpenPicker();
+        private static readonly FileSavePicker SavePicker = GetSavePicker();
+        
+        public static async Task<StorageFile> ShowOpenPicker()
+        {
+            return await OpenPicker.PickSingleFileAsync();
+        }
+
+        public static async Task<StorageFile> ShowSavePicker()
+        {
+            return await SavePicker.PickSaveFileAsync();
+        }
+
+        private static FileOpenPicker GetOpenPicker()
+        {
+            FileOpenPicker picker = new FileOpenPicker();
+            picker.FileTypeFilter.Add(".lrc");
+            picker.FileTypeFilter.Add(".txt");
+            return picker;
+        }
+
+        private static FileSavePicker GetSavePicker()
+        {
+            FileSavePicker picker = new FileSavePicker();
+            picker.FileTypeChoices.Add("LRC File", new List<string> {".lrc"});
+            return picker;
+        }
+    }
+}
