2db8135fa7e2411c74043ffe203c6ec36760e5a6
李继龙 <kljzndx@outlook.com>
2017-09-02 01:01:13 +0800

    Added Reverse Add Function

--- SimpleLyricsEditor.BLL/LyricsOperations/Add.cs
+++ SimpleLyricsEditor.BLL/LyricsOperations/Add.cs
@@ -7,32 +7,46 @@ namespace SimpleLyricsEditor.BLL.LyricsOperations
 {
     public class Add : LyricsOperationBase
     {
-        /// <param name="contents"></param>
-        /// <param name="insertIndex">为 -1 则添加到最后</param>
-        /// <param name="targetList"></param>
         /// <param name="time"></param>
-        public Add(TimeSpan time, string[] contents, int insertIndex, IList<Lyric> targetList)
+        /// <param name="content"></param>
+        /// <param name="insertIndex">为 -1 则添加到最后</param>
+        /// <param name="isReverseAdd"></param>
+        /// <param name="targetList"></param>
+        public Add(TimeSpan time, string content, int insertIndex, bool isReverseAdd, IList<Lyric> targetList)
         {
             Items = new List<Lyric>();
             InsertIndex = insertIndex;
+            IsReverseAdd = isReverseAdd;
             TargetList = targetList;
 
-            foreach (var str in contents)
+            foreach (var str in content.Split('\r'))
                 Items.Add(new Lyric(time, str.Trim()));
         }
 
         public IList<Lyric> Items { get; set; }
         public int InsertIndex { get; set; }
+        public bool IsReverseAdd { get; set; }
         public IList<Lyric> TargetList { get; set; }
 
         public override void Do()
         {
             if (InsertIndex.Equals(-1))
+            {
                 foreach (var lyric in Items)
-                    TargetList.Add(lyric);
+                {
+                    if (!IsReverseAdd)
+                        TargetList.Add(lyric);
+                    else
+                        TargetList.Insert(0, lyric);
+                }
+                return;
+            }
 
             foreach (var lyric in Items)
-                TargetList.Insert(InsertIndex, lyric);
+                if(!IsReverseAdd)
+                    TargetList.Insert(++InsertIndex, lyric);
+                else
+                    TargetList.Insert(--InsertIndex, lyric);
         }
 
         public override void Undo()
