5a86bc499fedc3c863ad91202bb52541a1885f5f
李继龙 <kljzndx@outlook.com>
2017-09-02 01:14:24 +0800

    Added Page

---
+++ SimpleLyricsEditor/Views/UiFramework.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace SimpleLyricsEditor.Views
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class UiFramework : Page
+    {
+        public UiFramework()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Settings_AppBarButton_Click(object sender, RoutedEventArgs e)
+        {
+            Root_SplitView.IsPaneOpen = !Root_SplitView.IsPaneOpen;
+        }
+    }
+}
