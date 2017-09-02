c1272b0564347da089847dba96ee81468cfe05de
李继龙 <kljzndx@outlook.com>
2017-09-02 19:43:06 +0800

    添加类

---
+++ SimpleLyricsEditor.DAL.Factory/LyricsTagFactory.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using HappyStudio.UwpToolsLibrary.Information;
+
+namespace SimpleLyricsEditor.DAL.Factory
+{
+    public static class LyricsTagFactory
+    {
+        public static List<LyricsTag> CreateTags()
+        {
+            var list = new List<LyricsTag>
+            {
+                new LyricsTag("ti"),
+                new LyricsTag("ar"),
+                new LyricsTag("al"),
+                new LyricsTag("by"),
+                new LyricsTag("re", AppInfo.Name + " for UWP"),
+                new LyricsTag("ve", AppInfo.Version)
+            };
+            return list;
+        }
+    }
+}
