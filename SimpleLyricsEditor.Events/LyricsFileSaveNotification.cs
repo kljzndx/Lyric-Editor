125339b171a25c8104fc2916744d39990dcc0a6e
李继龙 <kljzndx@outlook.com>
2017-09-02 10:04:10 +0800

    Added class

---
+++ SimpleLyricsEditor.Events/LyricsFileSaveNotification.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Windows.Storage;
+
+namespace SimpleLyricsEditor.Events
+{
+    public static class LyricsFileSaveNotification
+    {
+        public static event EventHandler<FileChangeEventArgs> RunSaved;
+
+        public static void SaveFile(StorageFile file)
+        {
+            RunSaved?.Invoke(null, new FileChangeEventArgs(file));
+        }
+    }
+}
