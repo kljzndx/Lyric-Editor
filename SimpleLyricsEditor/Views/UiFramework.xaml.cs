5c05f7ebe00cd5279aff48f32893bf4f504195a0
李继龙 <kljzndx@outlook.com>
2017-09-02 19:42:29 +0800

    添加打开按钮

--- SimpleLyricsEditor/Views/UiFramework.xaml.cs
+++ SimpleLyricsEditor/Views/UiFramework.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Storage;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -12,6 +13,8 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using SimpleLyricsEditor.BLL;
+using SimpleLyricsEditor.Events;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -22,6 +25,9 @@ namespace SimpleLyricsEditor.Views
     /// </summary>
     public sealed partial class UiFramework : Page
     {
+        private StorageFile _musicFile;
+        private StorageFile _lyricsFile;
+
         public UiFramework()
         {
             this.InitializeComponent();
@@ -30,6 +36,23 @@ namespace SimpleLyricsEditor.Views
         private void Settings_AppBarButton_Click(object sender, RoutedEventArgs e)
         {
             Root_SplitView.IsPaneOpen = !Root_SplitView.IsPaneOpen;
+        }
+
+        private async void OpenMusicFile_MenuFlyoutItem_Click(object sender, RoutedEventArgs e)
+        {
+            _musicFile = await MusicFileOpenPicker.PickFile();
+            if (_musicFile != null)
+                MusicFileNotification.ChangeFile(_musicFile);
+        }
+
+        private async void OpenLyricsFile_MenuFlyoutItem_Click(object sender, RoutedEventArgs e)
+        {
+            StorageFile file = await LyricsFileOpenPicker.PickFile();
+            if (file == null)
+                return;
+
+            _lyricsFile = file;
+            LyricsFileChangeNotification.ChangeFile(_lyricsFile);
         }
     }
 }
