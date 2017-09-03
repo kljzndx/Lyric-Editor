953bbb03689562d414d4805f177cf5a4f452d294
李继龙 <kljzndx@outlook.com>
2017-09-03 19:06:23 +0800

    Added EventArgs

---
+++ SimpleLyricsEditor.Events/MusicChangeEventArgs.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using SimpleLyricsEditor.DAL;
+
+namespace SimpleLyricsEditor.Events
+{
+    public class MusicChangeEventArgs : EventArgs
+    {
+        public MusicChangeEventArgs(Music source)
+        {
+            Source = source;
+        }
+
+        public Music Source { get; set; }
+    }
+}
---
+++ SimpleLyricsEditor.Events/PositionChangeEventArgs.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SimpleLyricsEditor.Events
+{
+    public class PositionChangeEventArgs : EventArgs
+    {
+        public PositionChangeEventArgs(bool isUserChange, TimeSpan position)
+        {
+            IsUserChange = isUserChange;
+            Position = position;
+        }
+
+        public bool IsUserChange { get; set; }
+        public TimeSpan Position { get; set; }
+    }
+}
