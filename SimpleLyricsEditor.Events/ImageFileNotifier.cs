4795f59172183ca3b0cb9ab5a623d3d1dfc24025
李继龙 <kljzndx@outlook.com>
2017-09-04 08:22:40 +0800

    Added class

---
+++ SimpleLyricsEditor.Events/ImageFileNotifier.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Windows.Storage;
+
+namespace SimpleLyricsEditor.Events
+{
+    public static class ImageFileNotifier
+    {
+        public static event EventHandler<FileChangeEventArgs> FileChanged;
+
+        public static void ChangeFile(StorageFile file)
+        {
+            FileChanged?.Invoke(null, new FileChangeEventArgs(file));
+        }
+    }
+}
