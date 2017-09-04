671adc6f2f79155dc67e5f172c0731ffd603ae0d
李继龙 <kljzndx@outlook.com>
2017-09-04 09:31:12 +0800

    Added control

---
+++ SimpleLyricsEditor.Control/BlurBackgroundImage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Windows.Storage;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media.Imaging;
+using SimpleLyricsEditor.Events;
+
+// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace SimpleLyricsEditor.Control
+{
+    public sealed partial class BlurBackgroundImage : UserControl
+    {
+        public static readonly DependencyProperty SourceProperty = DependencyProperty.Register(
+            "Source", typeof(BitmapSource), typeof(BlurBackgroundImage), new PropertyMetadata(new BitmapImage()));
+
+        public static readonly DependencyProperty BlurDegreeProperty = DependencyProperty.Register(
+            "BlurDegree", typeof(double), typeof(BlurBackgroundImage), new PropertyMetadata(5D));
+
+        public BlurBackgroundImage()
+        {
+            InitializeComponent();
+            ImageFileNotifier.FileChanged += ImageFileChanged;
+        }
+
+        public BitmapSource Source
+        {
+            get => (BitmapSource) GetValue(SourceProperty);
+            set => SetValue(SourceProperty, value);
+        }
+
+        public double BlurDegree
+        {
+            get => (double) GetValue(BlurDegreeProperty);
+            set => SetValue(BlurDegreeProperty, value);
+        }
+
+        public void SetSource(BitmapSource source)
+        {
+            Source = source;
+        }
+
+        private async void ImageFileChanged(object sender, FileChangeEventArgs e)
+        {
+            await Source.SetSourceAsync(await e.File.OpenAsync(FileAccessMode.Read));
+        }
+    }
+}
