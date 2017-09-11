3ba3c2fd1ddaddc6737bfdd35983c70104cb4c2b
李继龙 <kljzndx@outlook.com>
2017-09-11 14:05:28 +0800

    Fixed bug

--- SimpleLyricsEditor.BLL/LyricsOperations/Copy.cs
+++ SimpleLyricsEditor.BLL/LyricsOperations/Copy.cs
@@ -7,18 +7,21 @@ namespace SimpleLyricsEditor.BLL.LyricsOperations
 {
     public class Copy : LyricsChangeOperationBase
     {
+        private readonly List<Lyric> _addItems;
         private readonly TimeSpan _interpolation;
         private readonly bool _isBig;
+        private bool _isDoed;
 
         public Copy(IEnumerable<Lyric> items, TimeSpan targetTime, IList<Lyric> targetList) : base(items, targetList)
         {
+            _addItems = new List<Lyric>();
             TargetTime = targetTime;
 
             var oldTime = Items.First().Time;
             _isBig = oldTime > targetTime;
             _interpolation = _isBig ? oldTime - targetTime : targetTime - oldTime;
         }
-        
+
         public TimeSpan TargetTime { get; set; }
 
         public override void Do()
@@ -26,13 +29,17 @@ namespace SimpleLyricsEditor.BLL.LyricsOperations
             foreach (var lyric in Items)
             {
                 var time = _isBig ? lyric.Time - _interpolation : lyric.Time + _interpolation;
-                TargetList.Add(new Lyric(time, lyric.Content));
+                var addItem = new Lyric(time, lyric.Content);
+                if (!_isDoed)
+                    _addItems.Add(addItem);
+                TargetList.Add(addItem);
             }
+            _isDoed = true;
         }
 
         public override void Undo()
         {
-            foreach (var lyric in Items)
+            foreach (Lyric lyric in _addItems)
                 TargetList.Remove(lyric);
         }
     }
