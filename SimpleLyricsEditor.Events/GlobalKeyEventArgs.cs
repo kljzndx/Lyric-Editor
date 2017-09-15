ecef0a368ceb3ba9f11a7d53c06853fbc34b6ff5
李继龙 <kljzndx@outlook.com>
2017-09-15 13:34:01 +0800

    Add Global KeyDown and KeyUp Event

---
+++ SimpleLyricsEditor.Events/GlobalKeyEventArgs.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Windows.System;
+
+namespace SimpleLyricsEditor.Events
+{
+    public class GlobalKeyEventArgs : EventArgs
+    {
+        public GlobalKeyEventArgs(VirtualKey key, bool isPressCtrl, bool isPressShift)
+        {
+            Key = key;
+            IsPressCtrl = isPressCtrl;
+            IsPressShift = isPressShift;
+        }
+
+        public VirtualKey Key { get; set; }
+        public bool IsPressCtrl { get; set; }
+        public bool IsPressShift { get; set; }
+    }
+}
---
+++ SimpleLyricsEditor.Events/GlobalKeyNotifier.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Windows.System;
+
+namespace SimpleLyricsEditor.Events
+{
+    public static class GlobalKeyNotifier
+    {
+        public static event EventHandler<GlobalKeyEventArgs> KeyDown;
+        public static event EventHandler<GlobalKeyEventArgs> KeyUp;
+
+        public static void PressKey(VirtualKey key, bool isPressCtrl, bool isPressShift)
+        {
+            KeyDown?.Invoke(null, new GlobalKeyEventArgs(key, isPressCtrl, isPressShift));
+        }
+
+        public static void ReleaseKey(VirtualKey key, bool isPressCtrl, bool isPressShift)
+        {
+            KeyUp?.Invoke(null, new GlobalKeyEventArgs(key, isPressCtrl, isPressShift));
+        }
+    }
+}
