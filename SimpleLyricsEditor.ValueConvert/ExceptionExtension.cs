0e9ce58bb1388776a12c034f6f960cbefc6197b0
李继龙 <kljzndx@outlook.com>
2017-10-04 09:11:22 +0800

    为 Exception 类添加扩展方法

---
+++ SimpleLyricsEditor.ValueConvert/ExceptionExtension.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+
+namespace SimpleLyricsEditor.ValueConvert
+{
+    public static class ExceptionExtension
+    {
+        public static string ToLongString(this Exception ex)
+        {
+            StringBuilder builder = new StringBuilder();
+
+            builder.AppendLine($"Error code: {ex.HResult:X}");
+            builder.AppendLine($"Error Info: {ex.Message}");
+            builder.AppendLine($"Error source: {ex.Source}");
+            builder.AppendLine($"Help link: {ex.HelpLink}");
+            builder.AppendLine($"Ohter message: {ex.Data}");
+            builder.AppendLine("Stack trare:");
+            builder.AppendLine(ex.StackTraceEx());
+
+            return builder.ToString();
+        }
+    }
+}
