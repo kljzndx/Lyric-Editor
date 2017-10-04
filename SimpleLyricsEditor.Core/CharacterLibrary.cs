ba3eb3ef02a79fd3113deca4ebe749a6040935e0
李继龙 <kljzndx@outlook.com>
2017-10-04 14:03:32 +0800

    完成异常信息本地化

--- SimpleLyricsEditor.Core/CharacterLibrary.cs
+++ SimpleLyricsEditor.Core/CharacterLibrary.cs
@@ -4,6 +4,7 @@ namespace SimpleLyricsEditor.Core
 {
     public static class CharacterLibrary
     {
+        public static ResourceLoader Error { get; } = ResourceLoader.GetForCurrentView("Error");
         public static ResourceLoader SettingsRoot { get; } = ResourceLoader.GetForCurrentView("SettingsRoot");
         public static ResourceLoader BackgroundSettings { get; } = ResourceLoader.GetForCurrentView("BackgroundSettings");
         public static ResourceLoader LyricsPreviewSettings { get; } = ResourceLoader.GetForCurrentView("LyricsPreviewSettings");
--- SimpleLyricsEditor.ValueConvert/ExceptionExtension.cs
+++ SimpleLyricsEditor.ValueConvert/ExceptionExtension.cs
@@ -1,20 +1,31 @@
 ﻿using System;
 using System.Text;
+using SimpleLyricsEditor.Core;
 
 namespace SimpleLyricsEditor.ValueConvert
 {
     public static class ExceptionExtension
     {
+        public static string ToShortString(this Exception ex)
+        {
+            StringBuilder builder = new StringBuilder();
+
+            builder.AppendLine($"{CharacterLibrary.Error.GetString("Code")} 0x{ex.HResult:X}");
+            builder.AppendLine($"{CharacterLibrary.Error.GetString("Information")} {ex.Message}");
+
+            return builder.ToString();
+        }
+
         public static string ToLongString(this Exception ex)
         {
             StringBuilder builder = new StringBuilder();
 
-            builder.AppendLine($"Error code: {ex.HResult:X}");
-            builder.AppendLine($"Error Info: {ex.Message}");
-            builder.AppendLine($"Error source: {ex.Source}");
-            builder.AppendLine($"Help link: {ex.HelpLink}");
-            builder.AppendLine($"Ohter message: {ex.Data}");
-            builder.AppendLine("Stack trare:");
+            builder.AppendLine($"{CharacterLibrary.Error.GetString("Code")} 0x{ex.HResult:X}");
+            builder.AppendLine($"{CharacterLibrary.Error.GetString("Information")} {ex.Message}");
+            builder.AppendLine($"{CharacterLibrary.Error.GetString("Source")} {ex.Source}");
+            builder.AppendLine($"{CharacterLibrary.Error.GetString("HelpLink")}: {ex.HelpLink}");
+            builder.AppendLine($"{CharacterLibrary.Error.GetString("Ohter")} {ex.Data}");
+            builder.AppendLine(CharacterLibrary.Error.GetString("StackTrace"));
             builder.AppendLine(ex.StackTraceEx());
 
             return builder.ToString();
