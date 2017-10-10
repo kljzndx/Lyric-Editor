8ad8fb52236bcb0d260b9df6085417fdbccb425f
李继龙 <kljzndx@outlook.com>
2017-10-10 14:26:08 +0800

    Added class "UpdateLogFilesIO"

---
+++ SimpleLyricsEditor.BLL/UpdateLogFilesIO.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+using Windows.Storage;
+
+namespace SimpleLyricsEditor.BLL
+{
+    public static class UpdateLogFilesIO
+    {
+        private static readonly Uri AllLogsFolderUri = new Uri("ms-appx:///Data/UpdateLogs/");
+        private static readonly Uri AllLogsFileUri = new Uri(AllLogsFolderUri, "AllLogsPath.json");
+        
+        public static async Task<string> GetAllLogsJson()
+        {
+            StorageFile file = await StorageFile.GetFileFromApplicationUriAsync(AllLogsFileUri);
+            return await FileIO.ReadTextAsync(file);
+        }
+
+        public static async Task<string> GetLogContent(string fileName)
+        {
+            StorageFile file = await StorageFile.GetFileFromApplicationUriAsync(new Uri(AllLogsFolderUri, fileName));
+            return await FileIO.ReadTextAsync(file);
+        }
+    }
+}
