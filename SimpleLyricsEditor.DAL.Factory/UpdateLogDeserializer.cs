3cb48aad21cb4516cd5976cc26b855fca7683ee0
李继龙 <kljzndx@outlook.com>
2017-10-10 14:28:01 +0800

    Adde class "UpdateLogDeserializer"

---
+++ SimpleLyricsEditor.DAL.Factory/UpdateLogDeserializer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using Windows.Data.Json;
+
+namespace SimpleLyricsEditor.DAL.Factory
+{
+    public class UpdateLogDeserializer
+    {
+        public static IEnumerable<UpdateLog> Deserialization(string json)
+        {
+            List<UpdateLog> logs = new List<UpdateLog>();
+            JsonArray datas = JsonArray.Parse(json);
+
+            foreach (IJsonValue items in datas)
+            {
+                JsonObject data = items.GetObject();
+                string version = data["Version"].GetString();
+                string date = data["Date"].GetString();
+                string file = data["File"].GetString();
+
+                logs.Add(new UpdateLog(version, date, file));
+            }
+
+            return logs;
+        }
+    }
+}
