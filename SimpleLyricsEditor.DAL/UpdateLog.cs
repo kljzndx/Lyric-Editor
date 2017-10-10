5c24736eff53d868f71ab266eaa5007f8aa717a7
李继龙 <kljzndx@outlook.com>
2017-10-10 13:30:30 +0800

    更正地址

--- SimpleLyricsEditor.DAL/UpdateLog.cs
+++ SimpleLyricsEditor.DAL/UpdateLog.cs
@@ -6,17 +6,16 @@ namespace SimpleLyricsEditor.DAL
 {
     public class UpdateLog : INotifyPropertyChanged
     {
-        public static readonly Uri AllLogsFileUri = new Uri("ms-appx:///Data/UpdateLog/AllLogsPath.json");
-
-        private static readonly Uri AllLogsFolderUri = new Uri("ms-appx:///Data/UpdateLog");
-
+        private static readonly Uri AllLogsFolderUri = new Uri("ms-appx:///Data/UpdateLogs/");
+        public static readonly Uri AllLogsFileUri = new Uri(AllLogsFolderUri, "AllLogsPath.json");
+        
         private string _content;
 
-        public UpdateLog(string version, string date, string path)
+        public UpdateLog(string version, string date, string file)
         {
             Version = version;
             Date = date;
-            Path = new Uri(AllLogsFolderUri, path);
+            Path = new Uri(AllLogsFolderUri, file);
         }
 
         public string Version { get; }
