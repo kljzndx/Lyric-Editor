203c176ff00ed8da3838453b6d09f3eac2c1e531
李继龙 <kljzndx@outlook.com>
2017-10-12 13:41:11 +0800

    准备快捷键大全

---
+++ SimpleLyricsEditor.DAL.Factory/ShortcutKeysDeserializer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml.Linq;
+using Windows.Data.Xml.Dom;
+
+namespace SimpleLyricsEditor.DAL.Factory
+{
+    public static class ShortcutKeysDeserializer
+    {
+        public static IDictionary<string, IEnumerable<ShortcutKey>> Deserialization(string xml)
+        {
+            var result = new Dictionary<string, IEnumerable<ShortcutKey>>();
+            List<ShortcutKey> currentClassList = new List<ShortcutKey>();
+            XDocument xd = XDocument.Parse(xml);
+            var allKeys = xd.Element("ShortcutKeys");
+            var keys = allKeys.Elements("ShortcutKey");
+            string currentClassName = String.Empty;
+
+            foreach (XElement key in keys)
+            {
+                string className = key.Attribute("Class").Value.Trim();
+                string condition = key.Element("Condition").Value.Trim();
+                string function = key.Element("Function").Value.Trim();
+
+                if (!className.Equals(currentClassName))
+                {
+                    result.Add(currentClassName, currentClassList);
+                    currentClassName = className;
+                    currentClassList.Clear();
+                }
+
+                currentClassList.Add(new ShortcutKey(condition, function));
+            }
+
+            result.Add(currentClassName, currentClassList);
+            return result;
+        }
+    }
+}
---
+++ SimpleLyricsEditor.DAL/ShortcutKey.cs
@@ -0,0 +1,14 @@
+﻿namespace SimpleLyricsEditor.DAL
+{
+    public class ShortcutKey
+    {
+        public ShortcutKey(string condition, string function)
+        {
+            Condition = condition;
+            Function = function;
+        }
+
+        public string Condition { get; set; }
+        public string Function { get; set; }
+    }
+}
