7ee17efdec3c9bffa83f59079e487c72ab837bf5
李继龙 <kljzndx@outlook.com>
2017-10-14 23:34:54 +0800

    准备快捷键文档读取器

---
+++ SimpleLyricsEditor.BLL/ShortcutKeysDocumentFileReader.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading.Tasks;
+using Windows.Storage;
+
+namespace SimpleLyricsEditor.BLL
+{
+    public class ShortcutKeysDocumentFileReader
+    {
+        public async Task<string> GetFileContent()
+        {
+            StorageFile file = await StorageFile.GetFileFromApplicationUriAsync(new Uri("ms-appx:///Data/ShortcutKeysDocument/Shortcut Keys Document.xml"));
+            return await FileIO.ReadTextAsync(file);
+        }
+    }
+}
