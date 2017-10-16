1137ce2cf93c9a83717ecf3863be45c3f945edb4
李继龙 <kljzndx@outlook.com>
2017-10-16 23:48:33 +0800

    完成毛玻璃效果部署

--- SimpleLyricsEditor.Core/Settings.cs
+++ SimpleLyricsEditor.Core/Settings.cs
@@ -11,6 +11,9 @@ namespace SimpleLyricsEditor.Core
         [SettingFieldByNormal(nameof(Balance), 0D)] private double _balance;
         [SettingFieldByNormal(nameof(Volume), 1D)] private double _volume;
 
+        [SettingFieldByNormal(nameof(FrostedGlassVisibility), true)] private bool _frostedGlassVisibility;
+        [SettingFieldByNormal(nameof(FrostedGlassOpacity), 0.4D)] private double _frostedGlassOpacity;
+
         [SettingFieldByEnum(nameof(PageTheme), typeof(ElementTheme), nameof(ElementTheme.Dark))] private ElementTheme _pageTheme;
         [SettingFieldByNormal(nameof(BackgroundVisibility), true)] private bool _backgroundVisibility;
         [SettingFieldByNormal(nameof(IsFollowSongAlbumCover),  true)] private bool _isFollowSongAlbumCover;
@@ -63,6 +66,18 @@ namespace SimpleLyricsEditor.Core
         public bool IsLightTheme => PageTheme == ElementTheme.Light || 
                                     PageTheme == ElementTheme.Default && Application.Current.RequestedTheme == ApplicationTheme.Light;
         
+        public bool FrostedGlassVisibility
+        {
+            get => _frostedGlassVisibility;
+            set => SetSetting(ref _frostedGlassVisibility, value);
+        }
+
+        public double FrostedGlassOpacity
+        {
+            get => _frostedGlassOpacity;
+            set => SetSetting(ref _frostedGlassOpacity, value);
+        }
+
         public bool BackgroundVisibility
         {
             get => _backgroundVisibility;
@@ -73,7 +88,7 @@ namespace SimpleLyricsEditor.Core
             get => _isFollowSongAlbumCover;
             set => SetSetting(ref _isFollowSongAlbumCover, value);
         }
-        
+
         public string BackgroundImagePath
         {
             get => _backgroundImagePath;
