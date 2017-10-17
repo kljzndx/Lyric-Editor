7d75b76c66a67ad46c3bb91dd424b931dda213a0
李继龙 <kljzndx@outlook.com>
2017-10-18 07:16:10 +0800

    响应vm的更改

--- SimpleLyricsEditor/Views/MainPage.xaml.cs
+++ SimpleLyricsEditor/Views/MainPage.xaml.cs
@@ -285,7 +285,7 @@ namespace SimpleLyricsEditor.Views
 
         private void LyricsSort_Button_Click(object sender, RoutedEventArgs e)
         {
-            _viewModel.Sort();
+            _viewModel.Sort(_viewModel.LyricItems);
         }
 
         #endregion
