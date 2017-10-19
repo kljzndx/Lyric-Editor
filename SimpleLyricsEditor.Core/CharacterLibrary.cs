f9e3acff7c80777ef85820961b4c28c8d033c90f
李继龙 <kljzndx@outlook.com>
2017-10-19 09:39:05 +0800

    将 "Error" 字符库重命名为 "ErrorTable"

--- SimpleLyricsEditor.Core/CharacterLibrary.cs
+++ SimpleLyricsEditor.Core/CharacterLibrary.cs
@@ -5,7 +5,7 @@ namespace SimpleLyricsEditor.Core
     public static class CharacterLibrary
     {
         public static ResourceLoader MessageBox { get; } = ResourceLoader.GetForCurrentView("MessageBox");
-        public static ResourceLoader Error { get; } = ResourceLoader.GetForCurrentView("Error");
+        public static ResourceLoader ErrorTable { get; } = ResourceLoader.GetForCurrentView("ErrorTable");
         public static ResourceLoader SettingsRoot { get; } = ResourceLoader.GetForCurrentView("SettingsRoot");
         public static ResourceLoader BackgroundSettings { get; } = ResourceLoader.GetForCurrentView("BackgroundSettings");
         public static ResourceLoader LyricsPreviewSettings { get; } = ResourceLoader.GetForCurrentView("LyricsPreviewSettings");
