c07942bb89e5da0b79f532059bdec10df80e5c82
李继龙 <kljzndx@outlook.com>
2017-10-26 17:14:04 +0800

    添加 “加入QQ群” 和 “Skype会话” 按钮

--- SimpleLyricsEditor/Views/AboutPage.xaml.cs
+++ SimpleLyricsEditor/Views/AboutPage.xaml.cs
@@ -13,6 +13,7 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 using HappyStudio.UwpToolsLibrary.Information;
+using SimpleLyricsEditor.Core;
 
 // https://go.microsoft.com/fwlink/?LinkId=234238 上介绍了“空白页”项模板
 
@@ -28,6 +29,22 @@ namespace SimpleLyricsEditor.Views
         public AboutPage()
         {
             this.InitializeComponent();
+
+            JoinQqGroup_HyperlinkButton.AddHandler(PointerEnteredEvent, new PointerEventHandler(JoinQqGroup_HyperlinkButton_PointerEntered), true);
+            JoinQqGroup_HyperlinkButton.AddHandler(PointerExitedEvent, new PointerEventHandler(JoinQqGroup_HyperlinkButton_PointerCanceled), true);
+            JoinQqGroup_HyperlinkButton.AddHandler(PointerReleasedEvent, new PointerEventHandler(JoinQqGroup_HyperlinkButton_PointerCanceled), true);
+            JoinQqGroup_HyperlinkButton.AddHandler(PointerCanceledEvent, new PointerEventHandler(JoinQqGroup_HyperlinkButton_PointerCanceled), true);
+            JoinQqGroup_HyperlinkButton.AddHandler(PointerCaptureLostEvent, new PointerEventHandler(JoinQqGroup_HyperlinkButton_PointerCanceled), true);
+        }
+
+        private void JoinQqGroup_HyperlinkButton_PointerEntered(object sender, PointerRoutedEventArgs e)
+        {
+            JoinQqGroup_TextBlock.Text = CharacterLibrary.About.GetString("JoinQqGroup");
+        }
+
+        private void JoinQqGroup_HyperlinkButton_PointerCanceled(object sender, PointerRoutedEventArgs e)
+        {
+            JoinQqGroup_TextBlock.Text = "184437847";
         }
     }
 }
