5ecd85598fdc6f2c12a1945388c5c1f17b36fb13
李继龙 <kljzndx@outlook.com>
2017-10-28 14:20:47 +0800

    Add Converter

---
+++ SimpleLyricsEditor.ValueConvert/BoolNullableToBool.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Windows.UI.Xaml.Data;
+
+namespace SimpleLyricsEditor.ValueConvert
+{
+    public class BoolNullableToBool : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            return (bool) value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            return value;
+        }
+    }
+}
