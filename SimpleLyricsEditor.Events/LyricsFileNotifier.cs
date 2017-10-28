92d3504268dabd01db4042845ead74074a545b0c
李继龙 <kljzndx@outlook.com>
2017-10-28 11:16:06 +0800

    将 “歌词文件更改” 和 “歌词文件保存” 通知器 合并为 “歌词文件” 通知器

--- SimpleLyricsEditor.Events/LyricsFileNotifier.cs
+++ SimpleLyricsEditor.Events/LyricsFileNotifier.cs
@@ -3,10 +3,16 @@ using Windows.Storage;
 
 namespace SimpleLyricsEditor.Events
 {
-    public static class LyricsFileSaveNotifier
+    public static class LyricsFileNotifier
     {
+        public static event EventHandler<FileChangeEventArgs> FileChanged;
         public static event EventHandler<FileChangeEventArgs> SaveRequested;
-
+        
+        public static void ChangeFile(StorageFile file)
+        {
+            FileChanged?.Invoke(null, new FileChangeEventArgs(file));
+        }
+        
         public static void SendSaveRequest(StorageFile file)
         {
             SaveRequested?.Invoke(null, new FileChangeEventArgs(file));
