40bf931a80e57b40deb7f03bbd573b14ed388466
李继龙 <kljzndx@outlook.com>
2017-10-30 07:28:40 +0800

    Sort

--- SimpleLyricsEditor.Control/InputContentDialog.xaml.cs
+++ SimpleLyricsEditor.Control/InputContentDialog.xaml.cs
@@ -1,17 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 
 // The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
 
@@ -22,15 +11,15 @@ namespace SimpleLyricsEditor.Control
         public static readonly DependencyProperty UserInputProperty = DependencyProperty.Register(
             nameof(UserInput), typeof(string), typeof(InputContentDialog), new PropertyMetadata(String.Empty));
 
+        public InputContentDialog()
+        {
+            InitializeComponent();
+        }
+
         public string UserInput
         {
             get => (string) GetValue(UserInputProperty);
             set => SetValue(UserInputProperty, value);
-        }
-
-        public InputContentDialog()
-        {
-            this.InitializeComponent();
         }
     }
 }
