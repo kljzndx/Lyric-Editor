8663a4f763ce19914972d5edcaf57520098f4a9b
李继龙 <kljzndx@outlook.com>
2017-10-29 21:48:13 +0800

    添加 “输入对话框” 控件

---
+++ SimpleLyricsEditor.Control/InputContentDialog.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace SimpleLyricsEditor.Control
+{
+    public sealed partial class InputContentDialog : ContentDialog
+    {
+        public static readonly DependencyProperty UserInputProperty = DependencyProperty.Register(
+            nameof(UserInput), typeof(string), typeof(InputContentDialog), new PropertyMetadata(String.Empty));
+
+        public string UserInput
+        {
+            get => (string) GetValue(UserInputProperty);
+            set => SetValue(UserInputProperty, value);
+        }
+
+        public InputContentDialog()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
