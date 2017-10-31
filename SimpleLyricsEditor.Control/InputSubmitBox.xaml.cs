8aa2221c4670b232aa9b595344b57e207b825301
李继龙 <kljzndx@outlook.com>
2017-11-01 07:40:50 +0800

    将输入对话框改成输入提交框

---
+++ SimpleLyricsEditor.Control/InputSubmitBox.xaml.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
+
+// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace SimpleLyricsEditor.Control
+{
+    public sealed partial class InputSubmitBox : UserControl
+    {
+        public static readonly DependencyProperty UserInputProperty = DependencyProperty.Register(
+            nameof(UserInput), typeof(string), typeof(InputSubmitBox), new PropertyMetadata(String.Empty));
+
+        public static readonly DependencyProperty SubmitButtonContentProperty = DependencyProperty.Register(
+            nameof(SubmitButtonContent), typeof(string), typeof(InputSubmitBox), new PropertyMetadata('\uE10B'));
+
+        private bool _isInputBoxGotFocus;
+
+        public InputSubmitBox()
+        {
+            InitializeComponent();
+            InputBox_Border.Visibility = Visibility.Visible;
+        }
+
+        public string UserInput
+        {
+            get => (string) GetValue(UserInputProperty);
+            set => SetValue(UserInputProperty, value);
+        }
+
+        public string SubmitButtonContent
+        {
+            get => (string) GetValue(SubmitButtonContentProperty);
+            set => SetValue(SubmitButtonContentProperty, value);
+        }
+
+        public event EventHandler Submited;
+
+        private void ExpandInputBox()
+        {
+            Fold_Storyboard.Stop();
+            InputBox_Border.Visibility = Visibility.Visible;
+            Expand_Storyboard.Begin();
+        }
+
+        private void FoldInputBox()
+        {
+            if (!_isInputBoxGotFocus)
+                Fold_Storyboard.Begin();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Submited?.Invoke(this, EventArgs.Empty);
+        }
+        
+        private void Expand_Storyboard_Completed(object sender, object e)
+        {
+            TextBox.Focus(FocusState.Pointer);
+        }
+
+        private void Fold_Storyboard_Completed(object sender, object e)
+        {
+            InputBox_Border.Visibility = Visibility.Collapsed;
+        }
+
+        private void InputBox_Border_GotFocus(object sender, RoutedEventArgs e)
+        {
+            _isInputBoxGotFocus = true;
+        }
+
+        private void InputBox_Border_LostFocus(object sender, RoutedEventArgs e)
+        {
+            _isInputBoxGotFocus = false;
+        }
+
+        private void Root_Grid_LostFocus(object sender, RoutedEventArgs e)
+        {
+            Fold_Storyboard.Begin();
+        }
+        
+        private void Root_Grid_Holding(object sender, Windows.UI.Xaml.Input.HoldingRoutedEventArgs e)
+        {
+            ExpandInputBox();
+        }
+
+        private void Root_Grid_PointerEntered(object sender, Windows.UI.Xaml.Input.PointerRoutedEventArgs e)
+        {
+            ExpandInputBox();
+        }
+
+        private void Root_Grid_PointerExited(object sender, PointerRoutedEventArgs e)
+        {
+            FoldInputBox();
+        }
+
+        private void Root_Grid_PointerCanceled(object sender, PointerRoutedEventArgs e)
+        {
+            FoldInputBox();
+        }
+
+        private void Root_Grid_PointerCaptureLost(object sender, PointerRoutedEventArgs e)
+        {
+            FoldInputBox();
+        }
+    }
+}
