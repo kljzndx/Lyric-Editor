6299144ec2878df1aa81fc291cea0e0d37ef2cb6
李继龙 <kljzndx@outlook.com>
2017-11-02 13:10:20 +0800

    添加弹窗界面模型类

---
+++ SimpleLyricsEditor.DAL/DialogUserInterface.cs
@@ -0,0 +1,14 @@
+﻿namespace SimpleLyricsEditor.DAL
+{
+    public abstract class DialogUserInterface
+    {
+        protected DialogUserInterface(string title, string closeText)
+        {
+            Title = title;
+            CloseText = closeText;
+        }
+
+        public string Title { get; }
+        public string CloseText { get; }
+    }
+}
---
+++ SimpleLyricsEditor.DAL/ShortcutKeysDialogUI.cs
@@ -0,0 +1,14 @@
+﻿namespace SimpleLyricsEditor.DAL
+{
+    public class ShortcutKeysDialogUI : DialogUserInterface
+    {
+        public ShortcutKeysDialogUI(string title, string closeText, string conditionTag, string functionTag) : base(title, closeText)
+        {
+            ConditionTag = conditionTag;
+            FunctionTag = functionTag;
+        }
+
+        public string ConditionTag { get; }
+        public string FunctionTag { get; }
+    }
+}
