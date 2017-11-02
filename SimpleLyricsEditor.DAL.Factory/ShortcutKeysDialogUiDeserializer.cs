6078e0bc6088e380da41ac7e7bf90b666eab3b0b
李继龙 <kljzndx@outlook.com>
2017-11-02 13:12:13 +0800

    添加快捷键列表弹窗界面数据反序列化器类

---
+++ SimpleLyricsEditor.DAL.Factory/ShortcutKeysDialogUiDeserializer.cs
@@ -0,0 +1,20 @@
+﻿using System.Xml.Linq;
+
+namespace SimpleLyricsEditor.DAL.Factory
+{
+    public class ShortcutKeysDialogUiDeserializer
+    {
+        public ShortcutKeysDialogUI Deserialization(string xml)
+        {
+            XDocument doc = XDocument.Parse(xml);
+            XElement dialogUiElement = doc.Element("DialogUi");
+            return new ShortcutKeysDialogUI
+                (
+                    dialogUiElement.Element("Title").Value,
+                    dialogUiElement.Element("CloseButtonText").Value,
+                    dialogUiElement.Element("ConditionTag").Value,
+                    dialogUiElement.Element("FunctionTag").Value
+                );
+        }
+    }
+}
