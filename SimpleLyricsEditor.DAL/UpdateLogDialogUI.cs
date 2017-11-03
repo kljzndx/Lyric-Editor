fa9a135857498980e8d9a1cc0ab53df3386cada9
李继龙 <kljzndx@outlook.com>
2017-11-03 08:29:19 +0800

    Added Class "UpdateLogDialogUi"

---
+++ SimpleLyricsEditor.DAL/UpdateLogDialogUI.cs
@@ -0,0 +1,14 @@
+﻿namespace SimpleLyricsEditor.DAL
+{
+    public class UpdateLogDialogUI : DialogUserInterface
+    {
+        public UpdateLogDialogUI(string title, string closeText, string informationTag, string allVersionsTag) : base(title, closeText)
+        {
+            InformationTag = informationTag;
+            AllVersionsTag = allVersionsTag;
+        }
+
+        public string InformationTag { get; set; }
+        public string AllVersionsTag { get; set; }
+    }
+}
