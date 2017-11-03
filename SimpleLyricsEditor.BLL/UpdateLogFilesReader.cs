c58dd44160dc971bfcdf6519b56a9e90af79355f
李继龙 <kljzndx@outlook.com>
2017-11-03 08:31:51 +0800

    添加读取DialogUI.xml方法

--- SimpleLyricsEditor.BLL/UpdateLogFilesReader.cs
+++ SimpleLyricsEditor.BLL/UpdateLogFilesReader.cs
@@ -7,6 +7,12 @@ namespace SimpleLyricsEditor.BLL
     public class UpdateLogFilesReader
     {
         private readonly Uri _allLogsFolderUri = new Uri("ms-appx:///Data/UpdateLogs/");
+
+        public async Task<string> ReadDialogUI()
+        {
+            StorageFile file = await StorageFile.GetFileFromApplicationUriAsync(new Uri(_allLogsFolderUri, "DialogUI.xml"));
+            return await FileIO.ReadTextAsync(file);
+        }
         
         public async Task<string> GetAllLogsJson()
         {
