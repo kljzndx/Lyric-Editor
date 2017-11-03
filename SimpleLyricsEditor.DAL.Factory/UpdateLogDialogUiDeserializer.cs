2a874a6e6a1a1930c6e96fba2369ef79cbc56673
李继龙 <kljzndx@outlook.com>
2017-11-03 08:30:35 +0800

    添加UI数据反序列化器

---
+++ SimpleLyricsEditor.DAL.Factory/UpdateLogDialogUiDeserializer.cs
@@ -0,0 +1,20 @@
+﻿using System.Xml.Linq;
+
+namespace SimpleLyricsEditor.DAL.Factory
+{
+    public class UpdateLogDialogUiDeserializer
+    {
+        public UpdateLogDialogUI Deserialization(string xml)
+        {
+            XDocument doc = XDocument.Parse(xml);
+            XElement rootNode = doc.Root;
+            return new UpdateLogDialogUI
+            (
+                rootNode.Element("Title").Value,
+                rootNode.Element("CloseButtonText").Value,
+                rootNode.Element("InformationTag").Value,
+                rootNode.Element("AllVersionsTag").Value
+            );
+        }
+    }
+}
