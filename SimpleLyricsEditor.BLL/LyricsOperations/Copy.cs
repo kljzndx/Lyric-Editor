f9b2fb19fd874037a6f83f585cf0cc33e740579b
李继龙 <kljzndx@outlook.com>
2017-11-07 14:07:45 +0800

    完美支持向下添加

--- SimpleLyricsEditor.BLL/LyricsOperations/Copy.cs
+++ SimpleLyricsEditor.BLL/LyricsOperations/Copy.cs
@@ -9,6 +9,7 @@ namespace SimpleLyricsEditor.BLL.LyricsOperations
     {
         private readonly List<Lyric> _addItems;
         private readonly TimeSpan _interpolation;
+        private readonly int _insertId;
         private readonly bool _isBig;
         private bool _isDoed;
 
@@ -17,7 +18,16 @@ namespace SimpleLyricsEditor.BLL.LyricsOperations
             _addItems = new List<Lyric>();
             TargetTime = targetTime;
 
-            var oldTime = Items.First().Time;
+            for (var i = TargetList.Count - 1; i >= 0; i--)
+            {
+                if (Items.Contains(TargetList[i]))
+                {
+                    _insertId = i;
+                    break;
+                }
+            }
+
+            var oldTime = Positions.Values.First().Time;
             _isBig = oldTime > targetTime;
             _interpolation = _isBig ? oldTime - targetTime : targetTime - oldTime;
         }
@@ -26,13 +36,15 @@ namespace SimpleLyricsEditor.BLL.LyricsOperations
 
         public override void Do()
         {
-            foreach (var lyric in Items)
+            int insertId = _insertId + 1;
+
+            foreach (var lyric in Positions.Values)
             {
                 var time = _isBig ? lyric.Time - _interpolation : lyric.Time + _interpolation;
                 var addItem = new Lyric(time, lyric.Content);
                 if (!_isDoed)
                     _addItems.Add(addItem);
-                TargetList.Add(addItem);
+                TargetList.Insert(insertId++, addItem);
             }
 
             _isDoed = true;
