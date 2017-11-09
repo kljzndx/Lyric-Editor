440f279e99c2dcd2c637df7bc966e21f060ad1b2
李继龙 <kljzndx@outlook.com>
2017-11-09 08:01:26 +0800

    为设置页面弄了一个基类

---
+++ SimpleLyricsEditor/Models/SettingsPageBase.cs
@@ -0,0 +1,19 @@
+﻿using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media.Animation;
+using SimpleLyricsEditor.Core;
+
+namespace SimpleLyricsEditor.Models
+{
+    public abstract class SettingsPageBase : Page
+    {
+        protected Settings _settings = Settings.Current;
+
+        protected SettingsPageBase()
+        {
+            this.Transitions = new TransitionCollection
+            {
+                new NavigationThemeTransition()
+            };
+        }
+    }
+}
