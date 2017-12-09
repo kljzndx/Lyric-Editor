0cfce742500d42249e22bce691dc0216e1061279
李继龙 <kljzndx@outlook.com>
2017-12-10 07:41:31 +0800

    为String添加 HexColorToColor 方法

---
+++ SimpleLyricsEditor.ValueConvert/StringExtension.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using Windows.UI;
+
+namespace SimpleLyricsEditor.ValueConvert
+{
+    public static class StringExtension
+    {
+        public static Color HexColorToColor(this string hexColor)
+        {
+            Regex regex = new Regex(@"#(?<A>.{2})(?<R>.{2})(?<G>.{2})(?<B>.{2})");
+            Match match = regex.Match(hexColor);
+            List<byte> bytes = new List<byte>();
+
+            foreach (Group item in match.Groups)
+                bytes.Add(System.Convert.ToByte(item.Value, 16));
+
+            return Color.FromArgb(bytes[0], bytes[1], bytes[2], bytes[3]);
+        }
+    }
+}
