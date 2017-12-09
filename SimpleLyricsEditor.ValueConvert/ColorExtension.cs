23d505957d8c92ef476a1f0115f94522ae67d2b6
李继龙 <kljzndx@outlook.com>
2017-12-10 07:58:55 +0800

    为 Color 添加 ToHexString 方法

---
+++ SimpleLyricsEditor.ValueConvert/ColorExtension.cs
@@ -0,0 +1,12 @@
+﻿using Windows.UI;
+
+namespace SimpleLyricsEditor.ValueConvert
+{
+    public static class ColorExtension
+    {
+        public static string ToHexString(this Color color)
+        {
+            return $"#{color.A:X2}{color.R:X2}{color.G:X2}{color.B:X2}";
+        }
+    }
+}
