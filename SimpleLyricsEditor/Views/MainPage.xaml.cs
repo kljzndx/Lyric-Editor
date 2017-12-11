e7585987c686c7bd7fe1aa548f5a7996786927c8
李继龙 <kljzndx@outlook.com>
2017-12-11 19:04:49 +0800

    修复在没有进入多行编辑模式时点击歌词项的时间区域会报错的问题

--- SimpleLyricsEditor/Views/MainPage.xaml.cs
+++ SimpleLyricsEditor/Views/MainPage.xaml.cs
@@ -550,12 +550,16 @@ namespace SimpleLyricsEditor.Views
         {
             Lyric lyric = (sender as Button).DataContext as Lyric;
 
-            var range = new ItemIndexRange(Lyrics_ListView.Items.IndexOf(lyric), 1);
-            if (Lyrics_ListView.SelectedItems.Contains(lyric))
-                Lyrics_ListView.DeselectRange(range);
-            else
-                Lyrics_ListView.SelectRange(range);
-            
+            if (Lyrics_ListView.SelectionMode == ListViewSelectionMode.Multiple ||
+                Lyrics_ListView.SelectionMode == ListViewSelectionMode.Extended)
+            { 
+                var range = new ItemIndexRange(Lyrics_ListView.Items.IndexOf(lyric), 1);
+                if (Lyrics_ListView.SelectedItems.Contains(lyric))
+                    Lyrics_ListView.DeselectRange(range);
+                else
+                    Lyrics_ListView.SelectRange(range);
+            }
+
             GoToLyricTime(lyric);
         }
 
