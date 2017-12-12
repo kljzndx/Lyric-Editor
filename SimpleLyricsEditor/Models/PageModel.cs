effae6731d18e6bd88496c78293cb61d73ec7b14
李继龙 <kljzndx@outlook.com>
2017-12-12 17:19:15 +0800

    改造页面模型类，使其自动获取页面标题

--- SimpleLyricsEditor/Models/PageModel.cs
+++ SimpleLyricsEditor/Models/PageModel.cs
@@ -1,16 +1,42 @@
 ﻿using System;
+using System.Runtime.InteropServices;
+using System.Text.RegularExpressions;
+using Windows.ApplicationModel.Resources;
 
 namespace SimpleLyricsEditor.Models
 {
     public class PageModel
     {
-        public PageModel(string title, Type pageType)
+        public PageModel(Type pageType)
         {
-            Title = title;
+            Title = String.Empty;
             PageType = pageType;
+
+            Regex regex = new Regex(@"(?<pageName>.*)(Page)$");
+            Match match = regex.Match(PageType.Name);
+
+            if (match.Success)
+            {
+                string pageName = match.Groups["pageName"].Value;
+
+                try
+                {
+                    string pageTitle = ResourceLoader.GetForCurrentView(pageName).GetString("Title");
+
+                    if (!String.IsNullOrWhiteSpace(pageTitle))
+                        Title = pageTitle;
+                    else
+                        throw new Exception($"Title of \" {pageName} \" Page Not found");
+                }
+                catch (COMException)
+                {
+                    throw new Exception($"Resource Map of \" {pageName} \" Page Not found");
+                }
+            }
         }
 
-        public string Title { get; set; }
-        public Type PageType { get; set; }
+        public string Title { get; }
+        public Type PageType { get; }
+        
     }
 }
