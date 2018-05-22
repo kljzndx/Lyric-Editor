ffa467b894243683caf960f408838624a7604797
李继龙 <kljzndx@outlook.com>
2018-05-22 12:50:49 +0800

    为Color结构写了个转换到HSV的扩展方法

--- SimpleLyricsEditor.ValueConvert/ColorExtension.cs
+++ SimpleLyricsEditor.ValueConvert/ColorExtension.cs
@@ -1,4 +1,5 @@
-﻿using Windows.UI;
+﻿using System.Linq;
+using Windows.UI;
 
 namespace SimpleLyricsEditor.ValueConvert
 {
@@ -7,6 +8,36 @@ namespace SimpleLyricsEditor.ValueConvert
         public static string ToHexString(this Color color)
         {
             return $"#{color.A:X2}{color.R:X2}{color.G:X2}{color.B:X2}";
+        }
+
+        public static (double hue, double saturation, double value) ToHsv(this Color color)
+        {
+            double hue = 0D, saturation = 0D, value = 0D;
+            double r = color.R / 255D, g = color.G / 255D, b = color.B / 255D;
+
+            double max = new[] { r, g, b }.Max();
+            double min = new[] { r, g, b }.Min();
+            double delta = max - min;
+
+            saturation = max != 0 ? delta / max : 0;
+            value = max;
+
+            if (saturation == 0)
+                return (0, 0, 0);
+
+            if (r == max)
+                hue = ((g - b) / delta);
+            else if (g == max)
+                hue = ((b - r) / delta) + 2.0;
+            else if (b == max)
+                hue = ((r - g) / delta) + 4.0;
+
+            hue *= 60.0;
+
+            if (hue < 0)
+                hue += 360.0;
+
+            return (hue, saturation, value);
         }
     }
 }
