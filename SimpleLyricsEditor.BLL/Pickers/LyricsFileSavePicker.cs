2ad8085337dce041362a5992a0e23431d92afe09
李继龙 <kljzndx@outlook.com>
2019-10-28 19:26:34 +0800

    本地化默认文件名

--- SimpleLyricsEditor.BLL/Pickers/LyricsFileSavePicker.cs
+++ SimpleLyricsEditor.BLL/Pickers/LyricsFileSavePicker.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using Windows.ApplicationModel.Resources;
 using Windows.Storage;
 using Windows.Storage.Pickers;
 
@@ -8,11 +9,18 @@ namespace SimpleLyricsEditor.BLL.Pickers
 {
     public static class LyricsFileSavePicker
     {
+        private static readonly string DefaultFileName;
+
         public static readonly FileSavePicker Picker = GetPicker();
+
+        static LyricsFileSavePicker()
+        {
+            DefaultFileName = ResourceLoader.GetForCurrentView("MessageBox").GetString("DefaultFileName");
+        }
 
         public static async Task<StorageFile> PickFile(string fileName)
         {
-            Picker.SuggestedFileName = !String.IsNullOrWhiteSpace(fileName) ? fileName : "New lyrics";
+            Picker.SuggestedFileName = !String.IsNullOrWhiteSpace(fileName) ? fileName : DefaultFileName;
             return await Picker.PickSaveFileAsync();
         }
 
